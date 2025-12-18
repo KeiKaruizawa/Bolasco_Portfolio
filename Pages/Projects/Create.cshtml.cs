732e0bdd14cbf6c5d1cc407d75caa9cf10395f17
ACT-STUDENT <ACT-STUDENT@LAB4-PC44>
2025-12-18 17:13:24 +0800

    ubub

--- Pages/Projects/Create.cshtml.cs
+++ Pages/Projects/Create.cshtml.cs
@@ -1,19 +1,15 @@
 ﻿using BolascoProel4.Data;
-using BolascoProel4.Migrations;
 using BolascoProel4.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BolascoProel4.Pages.Projects
 {
     public class CreateModel : PageModel
     {
-        // Use the correct context name (not ContextModelSnapshot!)
         private readonly BolascoProel4Context _context;
 
         public CreateModel(BolascoProel4Context context)
@@ -24,7 +20,6 @@ namespace BolascoProel4.Pages.Projects
         [BindProperty]
         public Project Project { get; set; } = default!;
 
-        // This property receives the uploaded image file
         [BindProperty]
         public IFormFile? ImageFile { get; set; }
 
@@ -40,25 +35,17 @@ namespace BolascoProel4.Pages.Projects
                 return Page();
             }
 
-            // Convert uploaded image to byte array
             if (ImageFile != null && ImageFile.Length > 0)
             {
-                // Create a memory stream to read the file
                 using (var memoryStream = new MemoryStream())
                 {
-                    // Copy the uploaded file into memory
                     await ImageFile.CopyToAsync(memoryStream);
 
-                    // Convert to byte array and store in Project
-                    Project.ImageData = memoryStream.ToArray();
-
-                    // Store the image type (jpeg, png, etc.)
-                    // Using ImageType instead of ImageContentType
-                    Project.ImageType = ImageFile.ContentType;
+                    Project.ImageData = memoryStream.ToArray(); // Save bytes
+                    Project.ImageType = ImageFile.ContentType;  // Save mime type
                 }
             }
 
-            // Use the correct DbSet name
             _context.Project.Add(Project);
             await _context.SaveChangesAsync();
 
